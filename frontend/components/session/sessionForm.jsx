a9946e144ff75c3cff6002a7d8568e5b1f1d58ea
ryan-mapa <ryan.m.mapa@gmail.com>
2020-08-24 11:20:12 -0700

    Complete sessionForm functionality and fix self.find_by_credentials

--- frontend/components/session/sessionForm.jsx
+++ frontend/components/session/sessionForm.jsx
@@ -3,14 +3,35 @@ import React from 'react';
 class SessionForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {email: "", password: ""};
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleUpdate(type) {
+    return (e) => {
+      this.setState({ [type]: e.target.value })
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.processForm(this.state).then(() => this.props.history.push('/'));
   }
 
   render() {
     return (
       <div className="session-form-container">
         <h1>{this.props.formType === "login" ? "Log In" : "Sign Up"} Form</h1>
-        
+        <form onSubmit={this.handleSubmit}>
+          <label>Email:
+            <input type="email" onChange={this.handleUpdate("email")} value={this.state.email}/>
+          </label>
+          <label>Password:
+            <input type="password" onChange={this.handleUpdate("password")} value={this.state.password}/>
+          </label>
+          <input type="submit" value={this.props.formType === "login" ? "Log In" : "Sign Up"}/>
+        </form>
       </div>
     )
   }
